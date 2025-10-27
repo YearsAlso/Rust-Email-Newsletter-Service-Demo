e56a3c26ae8236d13e8e8f5ba28731544e98f25e
YearsAlso <1334713380@qq.com>
2025-10-27 13:46:10 +0800

    chore(deps): 更新依赖项并添加reqwest库
    - 将 http依赖项更新至版本 0.2.12- 将 h2依赖项更新至版本 0.3.27- 添加 reqwest 作为项目依赖，版本为 0.12.24
    - 添加了多个与网络请求相关的依赖项，包括 hyper、hyper-rustls、hyper-tls 等
    - 添加了 TLS 相关的依赖项，如 rustls、native-tls 和 openssl
    - 添加了系统配置和安全框架相关依赖，例如 security-framework 和 schannel
    - 添加了 WebAssembly 绑定相关依赖，如 wasm-bindgen 和 web-sys
    - 添加了 Windows 系统相关依赖，如 windows-sys 和 windows-registry
    - 添加了其他辅助库，如 atomic-waker、bumpalo、foreign-types 等

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::dev::Server;
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn heath_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+// #[actix_web::main]
+pub fn run() -> Result<Server,std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(|| async { HttpResponse::Ok().body("Hello world!") }))
+            .route("/{name}", web::get().to(greet))
+            .route("/health", web::get().to(heath_check))
+    })
+        .bind("127.0.0.1:8080")?
+        .run();
+    Ok(server)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,8 @@
 use actix_web::{web, App, HttpServer, HttpResponse, Responder, HttpRequest};
+use email_newsletter_service::run;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
 
-async fn heath_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(|| async { HttpResponse::Ok().body("Hello world!") }))
-            .route("/{name}", web::get().to(greet))
-            .route("/health", web::get().to(heath_check))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::heath_check;
-
-    #[tokio::test]
-    async fn heath_check_succeeds(){
-        let repose = heath_check().await ;
-        assert!(repose.status().is_success())
-    }
+    run()?.await
 }
